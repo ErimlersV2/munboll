adce3dd8f517a8ad0a291d866cf053c959a752bf
ATKN <ata2014kara2@gmail.com>
2019-12-16 04:09:49 +0300

    Git Commit AT 04:09 AM ?
    Fixed The Pages.
    Set up the google document linking
    Welcome Letter Changed
    Commitees Are Entered
    Faq Pages is Created. I remember those xD

---
+++ js/default.js
@@ -0,0 +1,30 @@
+function openerFunction(x){
+	if (document.getElementById(x).style.visibility == 'hidden')
+	{
+		document.getElementById(x).style.display='block';
+		setTimeout(function () {document.getElementById(x).style.visibility = 'visible';
+		document.getElementById(x).style.transform='scaleY(1)';
+		document.getElementById(x).style.height='auto';
+		document.getElementById(x).style.display='block';}, 50);
+		
+	} 
+	else{
+		document.getElementById(x).style.height='0px';
+		document.getElementById(x).style.transform='scaleY(0)';
+		document.getElementById(x).style.visibility = 'hidden';
+		setTimeout(() => document.getElementById(x).style.display='none',50);
+	}
+}
+function NarrowNavbarDrawerOpener()
+{
+	if(document.getElementById('smallMenues').style.visibility == 'hidden') {
+			document.getElementById('smallMenues').style.visibility='visible';
+			document.getElementById('smallMenues').style.width='40%';document.getElementById('NarrowButton').style.color='white';
+			document.getElementById('DrawerFiller').style.display="block";
+	}
+	else{
+		document.getElementById('smallMenues').style.visibility='hidden';
+		document.getElementById('smallMenues').style.width='0%';document.getElementById('NarrowButton').style.color='black';
+		document.getElementById('DrawerFiller').style.display="none";
+	}
+}
