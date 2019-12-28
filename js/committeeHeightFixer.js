7ea465b4e6189ca7098509387ec5a6f3de4e92fd
ATKN <ata2014kara2@gmail.com>
2019-12-29 02:13:09 +0300

    Fixed Selection Bug and writing errors

--- js/committeeHeightFixer.js
+++ js/committeeHeightFixer.js
@@ -1,7 +1,9 @@
 function FixHeight(){
 	document.getElementsByClassName("CommitteeSection")[0].style.height = document.getElementsByClassName("CommitteeText")[0].clientHeight+300;
-	if(document.getElementsByClassName("CommitteeText")[0].clientHeight < window.innerHeight-434){
-		document.getElementsByClassName("CommitteeSection") = window.innerHeight-434
+	if(document.getElementsByClassName("CommitteeText")[0].clientHeight < window.innerHeight-500){
+		document.getElementsByClassName("CommitteeSection").style.height = window.innerHeight;
+		document.getElementsByClassName("CommitteeText")[0].style.height = window.innerHeight;
+		document.getElementsByClassName("CommitteeImage")[0].style.height = window.innerHeight;
 	}
 }
 window.onload=FixHeight;
